54984353e3058410739bf7bf8fc242292607c9c7
odin <65809812@qq.com>
2015-09-07 12:43:23 +0800

    my first commit

---
+++ MyNewStart.sh
@@ -0,0 +1,14 @@
+Y|apt-get install vim
+Y|apt-get install emacs
+Y|apt-get install ssh
+Y|apt-get install subversion
+Y|apt-get install gcc
+Y|apt-get install g++
+Y|apt-get install gdb
+Y|apt-get install cmake
+Y|apt-get install python-dev
+Y|apt-get install ctags
+Y|apt-get install clang
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+svn cat http://code.taobao.org/svn/chd-cs-20120226-linux/trunk/.vimrc>.vimrc
+svn cat http://code.taobao.org/svn/chd-cs-20120226-linux/trunk/.zshrc>.zshrc
